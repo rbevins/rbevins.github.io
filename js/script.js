61cf0ac53bb81148f48d91f3bf0cb56b9875affd
rbevins <122995852+rbevins@users.noreply.github.com>
2023-01-18 10:15:12 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+function changeImage(a) {
+    document.getElementById("img").src=a;
+    document.getElementById("mainImageDisplay").style.display="block";
+    document.getElementById("mainVideoDisplay").style.display="none";
+    document.getElementById("video").src=a;
+    
+        }
+function changeVideo(a) {
+    document.getElementById("video").src=a;
+    document.getElementById("mainVideoDisplay").style.display="block";
+    document.getElementById("mainImageDisplay").style.display="none";
+        }
+function changeAlt(a) {
+    document.getElementById("img").alt=a;
+}
+
+function changeTextCity(){
+document.getElementById("outputD").innerHTML = "This piece is a 3-point perspective city I made. It was a drawing assignment in my Computer Animation and Web Design class. It was the final assignment in our mini unit about perspective. In the previous weeks, my class had learned about 1-point and 2-point perspective, and I was very excited to learn about 3-point perspective. I ended up really liking the piece I made. I thought the use of 3-point perspective helped to make the buildings look interesting and like they're towering high up into the sky as the viewer looks down on them. It was drawn traditionally using pencils on January 16th, 2022.";
+document.getElementById("outputN").innerHTML ="3-Point Perspective City";
+}
+function changeTextDragon(){
+    document.getElementById("outputD").innerHTML = "Dragon Kidnapping was a one-page comic I made in order to work on improving my paneling and composition skills for storytelling. I tried to make it so the panels flow well together in the proper order and in a way that enhances the short story being told. I was trying to make it so there was a lot of build up to make the subversion at the end hit properly. It was drawn on September 12th, 2022";
+    document.getElementById("outputN").innerHTML ="Dragon Kidnapping";
+    }
+function changeTextFish(){
+document.getElementById("outputD").innerHTML = "This Reef Animation was a 2 week-long storytelling/animation assignment from my Computer Animation and Web Design Class. It was a somewhat collaborative project, everyone in the class was supposed to model, texture, and rig at least 1 fish and 3 corals for the entire class to be able to use. The assets I modeled were the swordfish, the rocks, the crabs, the handcuffs, the pickaxes, the castle, the red coral, the magenta coral, the yellow coral, the harpoon, and the remote control. The other assets were made by my classmates. The story, the cinematography, the storyboarding, and all the animation was done by me. It was animated in Blender and completed on January 10th, 2022.";
+document.getElementById("outputN").innerHTML ="Reef Animation";
+}
+
+function changeTextMWD(){
+    document.getElementById("outputD").innerHTML = "This is one drawing from a series of Mother Ducking Witch pieces I've made. They were originally inspired by inside jokes from some of my friends relating to how I avoid swearing. I played into those jokes by continuing to censor myself in even wackier and comedic ways. One of those censors was Mother Ducking Witch, and I realized that it could be a fun character to draw. Which is what made me first draw a piece about a witch duck and her duckling. This specific piece was my 5th Mother Ducking Witch drawing, and my personal favorite. The drawing was made in Medibang on July 16th, 2021.";
+    document.getElementById("outputN").innerHTML ="Mother Ducking Witch";
+    }
+
+    function changeTextCrab(){
+        document.getElementById("outputD").innerHTML = "An animated meme I made using the song Crab Rave by Noisestorm. The grave and skeleton hand was actually from a previous animation I made in order to practice hand animation. All of the modeling, rigging, and animating was done by me, completed on December 12th, 2021. ";
+        document.getElementById("outputN").innerHTML ="Crab Grave";
+        }
+function changeTextBoxing(){
+    document.getElementById("outputD").innerHTML = "Match of the Century was a comic page I made with a specific goal in mind. I wanted to try and make a comic that was able to tell a story without using a single line of dialogue. I did this since it would force me to focus on the expressions and poses of the characters along with the layout and composition in order to deliver a story. I did use some text for sound effects and the victory announcement at the end, but those are just there to enhance the art in the panels rather than the main method of storytelling. Overall, I think the piece turned out well and definitely helped to improve my storytelling. It was completed on October 30th, 2022, in Krita.";
+    document.getElementById("outputN").innerHTML ="Match of the Century";
+    }
+function changeTextSoda(){
+    document.getElementById("outputD").innerHTML = "For this piece I wanted to do a still life from direct observation while also having some sort of storytelling aspect to go along with it. Luckily, we had some fairly full packs of Pepsi and Coca-Cola. So, after a bit of arranging them and taping the knives and forks on just right, I had the perfect set-up for a still life piece. It was a bit difficult since I don't have a lot of experience shading with colored pencils. But I felt like I really started to get the hang of it by the time I finished the piece. The drawing was done traditionally with pencil and colored pencils on October 23rd, 2022.";
+    document.getElementById("outputN").innerHTML ="Soda War";
+    }
+function changeTextKirby(){
+    document.getElementById("outputD").innerHTML = "For my Computer Animation and Web Design class, we had an assignment where we had to learn about a new aspect of Blender on our own. One of the options was learning about 2D animating in Blender, so I decided to do that. I chose to have it be about Kirby since he has a simple, easy to animate character design that would let me get a decent amount of animation done in the limitted time I had. It was completed on January 26th, 2022.";
+    document.getElementById("outputN").innerHTML ="Kirby Animation";
+    }
+function changeTextCow(){
+    document.getElementById("outputD").innerHTML = "This was a picture I took of some cows from a local farm. It was done for a Vermont themed photography assignment and I figured that cows would be the perfect subjects for one of them.";
+    document.getElementById("outputN").innerHTML ="Cow Collective";
+    }
+function changeTextZero(){
+    document.getElementById("outputD").innerHTML = "A modeling timelapse I did of Zero from the Mega Man Zero series. The footage had to be extremely sped up for the timelapse since my teacher wanted timelapses to be around a minute and a half. However, I had about 15 hours of footage, so it became a bit hard to follow in the final video.";
+    document.getElementById("outputN").innerHTML ="Zero Modeling Timelapse";
+    }
+function changeTextCube(){
+    document.getElementById("outputD").innerHTML = "The deleting of the default cube has been somewhat of an inside joke in the Blender community. So, for one of my assignments, I decided to make an animation where the default cube gets revenge. It was initially pretty difficult because I was trying to do the whole thing without changing the default viewing angle or order to keep it authentic. Then I realized I can move my view all I want because I can just make a new file afterward and append all of my work in order to keep the default angle.";
+    document.getElementById("outputN").innerHTML ="Cube's Revenge";
+    }
+function changeTextTrees(){
+    document.getElementById("outputD").innerHTML = "Another photo I took for my Vermont photography assignment. This one was meant to have more of a focus on Vermont foliage. I liked how the vibrant orange leaves leads the viewer's eyes down the path.";
+    document.getElementById("outputN").innerHTML ="Path of Orange";
+}
